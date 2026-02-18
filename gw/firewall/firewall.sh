8e19dcf0e7db7c02cbb7ead63e3b4ab20ef36fd8
Pedro <paguoll0612@g.educaan.es>
2026-02-18 10:47:15 +0100

    squid terminado

--- gw/firewall/firewall.sh
+++ gw/firewall/firewall.sh
@@ -1,128 +1,108 @@
-#/bin/bash
-set -x
-#Activar ip forwarding
+#!/bin/bash
+set -ex
+
+# Activar IP forwarding
 sysctl -w net.ipv4.ip_forward=1
 
-#Limpiar reglas previas
+# Limpiar reglas previas
 iptables -F
 iptables -t nat -F
 iptables -Z
 iptables -t nat -Z
 
-# ANTI-LOCK rule: Permitir ssh a traves de ETH0 para acceder con vagrant
+# ANTI-LOCK: Acceso SSH vía eth0 (Vagrant/Gestión externa)
 iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -j ACCEPT
 
-# Politica por defecto
+# POLÍTICAS POR DEFECTO
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
-##############################################
-#Reglas de proteccion local
-##############################################
-#L1. Permitir trafico de loopback
+###########################
+# Reglas de protección local (FIREWALL)
+###########################
+
+# L1. Loopback
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 
-#L2. Permitir ping a cualquier maquina interna y externa
+# L2. Ping saliente del firewall
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
-#L3. Permitir que me hagan ping desde LAN y DMZ
+# L3. Recibir ping desde LAN (eth3) y DMZ (eth2)
 iptables -A INPUT -i eth2 -s 172.1.1.0/24 -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -i eth3 -s 172.2.1.0/24 -p icmp --icmp-type echo-request -j ACCEPT
-iptables -A OUTPUT -o eth2 -s 172.1.1.1 -p icmp --icmp-type echo-reply -j ACCEPT
-iptables -A OUTPUT -o eth3 -s 172.2.1.1 -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -o eth2 -d 172.1.1.0/24 -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -o eth3 -d 172.2.1.0/24 -p icmp --icmp-type echo-reply -j ACCEPT
 
-#L4. Permitir consultas DNS
+# L4. DNS para el propio firewall
 iptables -A OUTPUT -o eth0 -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-#L5. Permitir http/https para actualizar y navegar
-iptables -A OUTPUT -o eth0 -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# L5. HTTP/HTTPS para el propio firewall (actualizaciones)
+iptables -A OUTPUT -o eth0 -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-#L6. Permitir solo acceso ssh desde adminpc
+# L6. SSH al firewall solo desde Admin PC
 iptables -A INPUT -i eth3 -s 172.2.1.10 -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth3 -d 172.2.1.10 -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-##############################################
-#Reglas de proteccion de red
-##############################################
-#R1. Se debe hacer NAT desde el trafico saliente
+
+############################
+# Reglas de protección de red (FORWARDING)
+############################
+
+# R1. NAT de salida para LAN y DMZ
 iptables -t nat -A POSTROUTING -s 172.2.1.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.1.1.0/24 -o eth0 -j MASQUERADE
 
-#R2. Permitir acceso desde la WAN a www a traves del 80 y 443 de www
-iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j DNAT --to 172.1.1.3:80
-iptables -A FORWARD -i eth1 -o eth2 -s 203.0.113.0/24 -d 172.1.1.3 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth1 -s 172.1.1.3 -d 203.0.113.0/24 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# R2. Port Forwarding (DNAT) de WAN a Servidor Web DMZ
+# Corregido: Si viene de WAN usualmente es eth0 o eth1 (usaré eth0 según lógica estándar de salida)
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 172.1.1.3:80
+iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.3 -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.3 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-#R3a. Usuarios de la LAN pueden acceder a 80 y 443 de www
+# R3.A. LAN puede ver la Web de la DMZ
 iptables -A FORWARD -i eth3 -o eth2 -s 172.2.1.0/24 -d 172.1.1.3 -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth3 -s 172.1.1.3 -d 172.2.1.0/24 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-#R3b. Admin accede por ssh al servidor
+# R3.B. Admin PC SSH a toda la DMZ
 iptables -A FORWARD -i eth3 -o eth2 -s 172.2.1.10 -d 172.1.1.0/24 -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth3 -s 172.1.1.0/24 -d 172.2.1.10 -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-
-#R4. Permitir salir trafico desde la LAN
-iptables -A FORWARD -i eth3 -o eth0 -s 172.2.1.0/24 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24  -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-#R4.v2.1. Trafico saliente ha d pasar por el proxy
+# R4. Tráfico de salida de la LAN
+# R4.1. LAN hacia Proxy SQUID en DMZ
 iptables -A FORWARD -i eth3 -o eth2 -s 172.2.1.0/24 -d 172.1.1.2 -p tcp --dport 3128 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth3 -s 172.1.1.2 -d 172.2.1.0/24 -p tcp --sport 3128 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#R4.v2.2. Permitir consultas DNS directas (sin proxy) tanto UDP (rapidas) como TCP (consultas grandes /  DNSSEC )
+
+# R4.2. DNS Directo desde LAN
 iptables -A FORWARD -i eth3 -o eth0 -s 172.2.1.0/24 -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED, RELATED -j ACCEPT
-iptables -A FORWARD -i eth3 -o eth0 -s 172.2.1.0/24 -p tdp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24 -p tdp --sport 53 -m conntrack --ctstate ESTABLISHED, RELATED -j ACCEPT
-#R4.v2.3 Permitir consultas NTP (reloj)
-iptables -A FORWARD -i eth3 -o eth0 -s 172.2.1.0/24 -p udp --dport 123 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24 -p udp --sport 123 -m conntrack --ctstate ESTABLISHED, RELATED -j ACCEPT
-#R4.v2.4 permitimos pings salientes para depuracion
+iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# R4.3. NTP y ICMP desde LAN
+iptables -A FORWARD -i eth3 -o eth0 -s 172.2.1.0/24 -p udp --dport 123 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24 -p udp --sport 123 -j ACCEPT
 iptables -A FORWARD -i eth3 -o eth0 -s 172.2.1.0/24 -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth3 -d 172.2.1.0/24 -p icmp --icmp-type echo-reply -j ACCEPT
 
+# R5. Tráfico de salida de la DMZ (DNS, NTP, HTTP/S para actualizaciones)
+iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p udp -m multiport --dports 53,123 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p udp -m multiport --sports 53,123 -j ACCEPT
 
-#R5. Permitir salir trafico de la DMZ (solo http/https/dns/ntp)
-iptables -t nat -A POSTROUTING -s 172.1.1.0/24 -o eth0 -j MASQUERADE
-#iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-#iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-#iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
-#iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-################################################
-# Reglas para el resto de prácticas
-################################################
-# Regla P6. Permitir acceso del proxy a Internet
-iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.2 -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-#iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.2 -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth2 -d 172.1.2.2 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-#P4 permitir acceso LDAP desde DMZ
+# P4. Acceso LDAP (DMZ -> LAN Server)
 iptables -A FORWARD -i eth2 -o eth3 -s 172.1.1.0/24 -d 172.2.1.2 -p tcp --dport 389 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth3 -o eth2 -s 172.2.1.2 -d 172.1.1.0/24 -p tcp --sport 389 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# DNS (puerto 53 UDP y TCP)
-# UDP es lo estándar para consultas
-iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-# TCP a veces es necesario para respuestas grandes o actualizaciones de zona
-iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-#NTP (puerto 123 UDP) para actualizar el reloj
-iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.0/24 -p udp --dport 123 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.0/24 -p udp --sport 123 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# P6. Permitimos salir a squid a 80 y 443
+iptables -A FORWARD -i eth2 -o eth0 -s 172.1.1.2 -p tcp -m multiport --dports 80,443 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth2 -d 172.1.1.2 -p tcp -m multiport --sports 80,443 -j ACCEPT
 
+
+echo "Configuración de iptables aplicada correctamente."
 
 ###### Logs para depurar
 iptables -A INPUT -j LOG --log-prefix "PJAO-INPUT: "
